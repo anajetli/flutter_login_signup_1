246d07ca12ca843a3709abbd8a957587c54ae5e4
Atif Naseem <atif@plego.com>
2023-06-12 23:31:12 +0500

    added const

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'Welcome/welcome_screen.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyApp(),
+    home: MyLogin(),
     routes: {
       'register': (context) => MyRegister(),
       'login': (context) => MyLogin(),
